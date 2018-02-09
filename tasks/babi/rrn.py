5dd86f7455f1d21247c5fffac8f142c9f8a8d580
Rasmus Berg Palm <rasmusbergpalm@gmail.com>
2018-02-09 10:16:31 +0100

    babi single, run 2

--- tasks/babi/rrn.py
+++ tasks/babi/rrn.py
@@ -18,7 +18,7 @@ from tasks.babi.data import bAbI
 
 
 class BaBiRecurrentRelationalNet(Model):
-    number = 3
+    number = 2
     devices = util.get_devices()
     revision = os.environ.get('REVISION')
     message = os.environ.get('MESSAGE')
