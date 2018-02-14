b6b33de3720b66c78aa725205ec84a60862da167
Rasmus Berg Palm <rasmusbergpalm@gmail.com>
2018-02-14 10:09:52 +0100

    babi average, run 21

--- tasks/babi/rrn.py
+++ tasks/babi/rrn.py
@@ -18,7 +18,7 @@ from tasks.babi.data import bAbI
 
 
 class BaBiRecurrentRelationalNet(Model):
-    number = 20
+    number = 21
     devices = util.get_devices()
     revision = os.environ.get('REVISION')
     message = os.environ.get('MESSAGE')
