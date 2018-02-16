a573475ddec27538918690e15156682ca11a2d5e
Rasmus Berg Palm <rasmusbergpalm@gmail.com>
2018-02-16 09:40:27 +0100

    babi avg

--- tasks/babi/test.py
+++ tasks/babi/test.py
@@ -3,7 +3,7 @@ import numpy as np
 
 from tasks.babi.rrn import BaBiRecurrentRelationalNet
 
-model_dir = '/home/rapal/runs/b6b33de/'
+model_dir = '/home/rapal/runs/fc9f60d/'
 n_steps = BaBiRecurrentRelationalNet.n_steps
 
 eval_fname = model_dir + '%d-eval.npz' % n_steps
