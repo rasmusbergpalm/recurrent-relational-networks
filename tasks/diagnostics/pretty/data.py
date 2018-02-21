f4710260d0c5417af2edbf02017db6e32a2cc020
Rasmus Berg Palm <rasmusbergpalm@gmail.com>
2018-02-21 11:55:43 +0100

    pretty-clevr 0 pre-generated, 4 step, 128

--- tasks/diagnostics/pretty/data.py
+++ tasks/diagnostics/pretty/data.py
@@ -50,9 +50,10 @@ class PrettyClevr:
             self.questions = [l.split(", ") for l in qf.readlines()]
 
     def sample_generator(self):
-        for img_fname, anchor, n_jumps, target in random.sample(self.questions, len(self.questions)):
-            if n_jumps == "0":
-                yield np.array(Image.open(self.data_dir + '/images/' + img_fname)), int(anchor), int(n_jumps), int(target)
+        while True:
+            for img_fname, anchor, n_jumps, target in random.sample(self.questions, len(self.questions)):
+                if n_jumps == "0":
+                    yield np.array(Image.open(self.data_dir + '/images/' + img_fname)), int(anchor), int(n_jumps), int(target)
 
 
 class PrettyClevrGenerator:
