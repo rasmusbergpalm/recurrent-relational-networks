beee0f5770128d616ca3258e21d48475e197f190
Rasmus Berg Palm <rasmusbergpalm@gmail.com>
2018-02-22 12:44:47 +0100

    generate pretty-clevr w. states

--- tasks/diagnostics/pretty/data.py
+++ tasks/diagnostics/pretty/data.py
@@ -71,65 +71,58 @@ class PrettyClevrGenerator:
 
     def sample_generator(self):
         while True:
-            object_colors = random.sample(range(self.n), self.n)
-            object_markers = random.sample(range(self.n), self.n)
-            objects = self.get_objects()
-            img = self._render(objects, object_colors, object_markers)
+            objects = [{'p': p.tolist(), 'c': c, 'm': m} for p, c, m in zip(self.get_object_positions(), random.sample(self.colors, len(self.colors)), self.markers)]
+            img = self._render(objects)
             questions = []
             for start_idx in range(self.n):
-                path = greedy(objects, self.n - 1, start_idx)
+                path = greedy(np.array([o['p'] for o in objects]), self.n - 1, start_idx)
                 for n_jumps in range(self.n):
                     for color_anchor in [True, False]:
-
                         target_obj = path[n_jumps]
                         if color_anchor:
-                            anchor = self.colors[object_colors[start_idx]]
-                            target = self.markers[object_markers[target_obj]]
+                            anchor = objects[start_idx]['c']
+                            target = objects[target_obj]['m']
                         else:
-                            anchor = self.markers[object_markers[start_idx]]
-                            target = self.colors[object_colors[target_obj]]
+                            anchor = objects[start_idx]['m']
+                            target = objects[target_obj]['c']
 
-                        questions.append((self.s2i[anchor], n_jumps, self.s2i[target]))
+                        questions.append((anchor, n_jumps, target))
 
-            yield img, questions
+            yield objects, img, questions
 
-    def get_objects(self):
+    def get_object_positions(self):
         objects = [np.random.uniform(size=(2,))]
         while len(objects) < self.n:
             o = np.random.uniform(size=(2,))
             if np.min(dist_squared(o, np.array(objects))) > self.r:
                 objects.append(o)
-        objects = np.array(objects)
         return objects
 
-    def _render(self, objects, object_colors, object_markers):
+    def _render(self, objects):
         fig = plt.figure(figsize=(1.28, 1.28), frameon=False)
         ax = fig.add_axes([0, 0, 1, 1])
         ax.axis('off')
         ax.set_xlim(0 - self.r, 1 + self.r)
         ax.set_ylim(0 - self.r, 1 + self.r)
-        colors = [self.colors[c_idx] for c_idx in object_colors]
-        markers = [self.markers[m_idx] for m_idx in object_markers]
-        for (x, y), c, m in zip(objects, colors, markers):
-            ax.scatter(x, y, c=c, marker=m)
+        for o in objects:
+            x, y = o['p']
+            ax.scatter(x, y, c=o['c'], marker=o['m'])
 
         return fig2array(fig)
 
     def generate(self, n):
         gen = self.sample_generator()
         with open('questions.csv', 'w') as qf:
-            qf.write("%s \n" % json.dumps(self.i2s))
-            qf.write("image, anchor, jumps, target\n")
             for i in range(n):
-                img, questions = next(gen)
+                objects, img, questions = next(gen)
                 png_name = '%05d.png' % i
+                json_name = '%05d.json' % i
+                with open(json_name, 'w') as fp:
+                    json.dump(objects, fp)
                 Image.fromarray(img).save(png_name)
-                qf.writelines(["%s, %d, %d, %d\n" % (png_name, a, b, c) for (a, b, c) in questions])
+                qf.writelines(["%s, %s, %s, %d, %s\n" % (png_name, json_name, a, b, c) for (a, b, c) in questions])
 
 
 if __name__ == '__main__':
-    d = PrettyClevr()
-    gen = d.sample_generator()
-    for i in range(10):
-        img, anchor, n_jumps, target = next(gen)
-        k = 0
+    d = PrettyClevrGenerator(8)
+    d.generate(10000)
