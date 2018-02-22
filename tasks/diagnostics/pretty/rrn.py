8403ba3a02048fc17c9b2c2f1637ee0825699b87
Rasmus Berg Palm <rasmusbergpalm@gmail.com>
2018-02-22 14:42:33 +0100

    pretty-clevr, 0, RN 128, bs 128

--- tasks/diagnostics/pretty/rrn.py
+++ tasks/diagnostics/pretty/rrn.py
@@ -16,7 +16,7 @@ import matplotlib.pyplot as plt
 
 
 class PrettyRRN(Model):
-    batch_size = 32
+    batch_size = 128
     revision = os.environ.get('REVISION')
     message = os.environ.get('MESSAGE')
     n_objects = 8
