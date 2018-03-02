efb8fc960a1ef3bf93fca990430bdc40e567abb3
Rasmus Berg Palm <rasmusbergpalm@gmail.com>
2018-03-02 14:11:20 +0100

    test MP

--- tests/test_message_passing.py
+++ tests/test_message_passing.py
@@ -13,15 +13,17 @@ class TestMessagePassing(TestCase):
         edges = tf.constant(np.array([[0, 1], [1, 2], [2, 1]]), tf.int32)
         edge_features = tf.zeros((3, 1), tf.float32)
 
-        def message_fn(x):
-            return x[:, 0:2] + x[:, 2:4]
-
-        out = message_passing(nodes, edges, edge_features, message_fn, 1.0)
         expected = np.array([
             [0, 0],  # no messages for node 0
             (x[0] + x[1]) + (x[2] + x[1]),  # 0 to 1, and 2 to 1
             (x[1] + x[2])  # 1 to 2
         ], dtype=np.float32)
+
+        def message_fn(x):
+            return x[:, 0:2] + x[:, 2:4]
+
+        with tf.device('/device:GPU:0'):
+            out = message_passing(nodes, edges, edge_features, message_fn, 1.0)
 
         with tf.Session().as_default():
             self.assertTrue(np.allclose(expected, out.eval()))
