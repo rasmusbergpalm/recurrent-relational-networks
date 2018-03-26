4e1d56c341004b436d5a140cdffed85c9c1a25b9
Rasmus Berg Palm <rasmusbergpalm@gmail.com>
2018-03-26 13:07:59 +0200

    babi ablation, no pre

--- tasks/babi/rrn.py
+++ tasks/babi/rrn.py
@@ -18,7 +18,7 @@ from tasks.babi.data import bAbI
 
 
 class BaBiRecurrentRelationalNet(Model):
-    number = 7
+    number = 1
     devices = util.get_devices()
     revision = os.environ.get('REVISION')
     message = os.environ.get('MESSAGE')
