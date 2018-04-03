ed0900f6d3506f99f1221167a630729cc9e798c5
Rasmus Berg Palm <rasmusbergpalm@gmail.com>
2018-04-03 14:49:30 +0200

    babi ablation, no dropout

--- tasks/babi/rrn.py
+++ tasks/babi/rrn.py
@@ -18,7 +18,7 @@ from tasks.babi.data import bAbI
 
 
 class BaBiRecurrentRelationalNet(Model):
-    number = 1
+    number = 2
     devices = util.get_devices()
     revision = os.environ.get('REVISION')
     message = os.environ.get('MESSAGE')
