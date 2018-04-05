70354b76477167c6d04fdcd84aa8285befeb61c5
Rasmus Berg Palm <rasmusbergpalm@gmail.com>
2018-04-05 16:02:00 +0200

    entailment, f(e(g1), e(g2)), forward

--- tasks/entailment/data.py
+++ tasks/entailment/data.py
@@ -2,11 +2,13 @@ import os
 import random
 import urllib.request
 import zipfile
+import tensorflow as tf
+import numpy as np
 
 from tasks.entailment.parser import Parser, propositional_language, ParseResult
 
 
-class Data:
+class Entailment:
     url = "https://www.dropbox.com/s/ufpau116axq92iz/entailment.zip?dl=1"
     base_dir = (os.environ.get('DATA_DIR') or "/tmp")
     zip_fname = base_dir + "/entailment.zip"
@@ -23,6 +25,28 @@ class Data:
             with zipfile.ZipFile(self.zip_fname) as f:
                 f.extractall(self.base_dir)
 
+    def batch_generator(self, fname, bs):
+        sg = self.sample_generator(fname)
+
+        def extend(batch_n, batch_e, segments, head, n, e, i):
+            batch_e.extend((np.array(e) + len(batch_n)).tolist())
+            batch_n.extend(n)
+            head.append(len(batch_n) - 1)
+            segments.extend([i] * len(n))
+
+        while True:
+            batch_n_a, batch_e_a, segments_a, head_a = [], [], [], []
+            batch_n_b, batch_e_b, segments_b, head_b = [], [], [], []
+            targets = []
+            for i in range(bs):
+                n_a, e_a, n_b, e_b, t = next(sg)
+                targets.append(t)
+
+                extend(batch_n_a, batch_e_a, segments_a, head_a, n_a, e_a, i)
+                extend(batch_n_b, batch_e_b, segments_b, head_b, n_b, e_b, i)
+
+            yield batch_n_a, batch_e_a, segments_a, head_a, batch_n_b, batch_e_b, segments_b, head_b, targets
+
     def sample_generator(self, fname):
         with open(self.dest_dir + '/' + fname, 'r') as fp:
             samples = [line.strip().split(',') for line in fp.readlines()]
@@ -34,7 +58,7 @@ class Data:
 
                 n_a, e_a = self.encode(a)
                 n_b, e_b = self.encode(b)
-                yield n_a, e_a, n_b, e_b, int(t)
+                yield n_a, e_a, n_b, e_b, float(t)
 
     def encode(self, p: ParseResult):
         edges = []
@@ -50,8 +74,16 @@ class Data:
         b = ParseResult(b.expression, [perm[op] if op in perm else op for op in b.ops], b.inputs)
         return a, b
 
+    def output_types(self):
+        #       batch_n_a,  batch_e_a,  segments_a,     head_a,     batch_n_b,  batch_e_b,  segments_b,     head_b,     targets
+        return  tf.int32,   tf.int32,   tf.int32,       tf.int32,   tf.int32,    tf.int32,  tf.int32,       tf.int32,   tf.float32
+
+    def output_shapes(self):
+        #       batch_n_a,  batch_e_a,  segments_a,     head_a,     batch_n_b,  batch_e_b,  segments_b,     head_b,     targets
+        return  (None,),      (None, 2),      (None,),        (None,),      (None,),      (None, 2),      (None,),        (None,),      (None,)
+
 
 if __name__ == '__main__':
-    d = Data()
-    for n1, e1, n2, e2, t in d.sample_generator('train.txt'):
+    d = Entailment()
+    for n1, e1, s1, h1, n2, e2, s2, h2, t in d.batch_generator('train.txt', 4):
         i = 0
---
+++ tasks/entailment/rrn.py
@@ -0,0 +1,130 @@
+import os
+
+import matplotlib
+import tensorflow as tf
+from tensorflow.contrib import layers
+from tensorflow.python.data import Dataset
+
+import util
+from message_passing import message_passing
+from model import Model
+from tasks.entailment.data import Entailment
+
+matplotlib.use('Agg')
+
+
+class EntailmentRRN(Model):
+    number = 1
+    batch_size = 128
+    data = Entailment()
+    n_steps = 16
+    n_hidden = 128
+    revision = os.environ.get('REVISION')
+    message = os.environ.get('MESSAGE')
+
+    def __init__(self):
+        super().__init__()
+        self.name = "%s %s" % (self.revision, self.message)
+
+        print("Building graph...")
+        self.session = tf.Session(config=tf.ConfigProto(allow_soft_placement=False))
+        self.global_step = tf.Variable(initial_value=0, trainable=False)
+        self.optimizer = tf.train.AdamOptimizer(1e-4)
+        self.is_training_ph = tf.placeholder(bool, name='is_training')
+        regularizer = layers.l2_regularizer(0.)
+
+        train_iterator = self._iterator(lambda: self.data.batch_generator('train.txt', self.batch_size), self.data.output_types(), self.data.output_shapes())
+        dev_iterator = self._iterator(lambda: self.data.batch_generator('validate.txt', self.batch_size), self.data.output_types(), self.data.output_shapes())
+
+        def mlp(x, scope, n_layers=1, n_hid=self.n_hidden, n_out=self.n_hidden, keep_prob=1.0):
+            with tf.variable_scope(scope):
+                for i in range(n_layers):
+                    x = layers.fully_connected(x, n_hid, weights_regularizer=regularizer)
+                x = layers.dropout(x, keep_prob=keep_prob, is_training=self.is_training_ph)
+                return layers.fully_connected(x, n_out, weights_regularizer=regularizer, activation_fn=None)
+
+        nodes_a, edges_a, segments_a, heads_a, nodes_b, edges_b, segments_b, heads_b, targets = tf.cond(
+            self.is_training_ph,
+            true_fn=lambda: train_iterator.get_next(),
+            false_fn=lambda: dev_iterator.get_next(),
+        )
+
+        def encode_graph(nodes, edges, segments, heads, reuse):
+            edge_features = tf.zeros((tf.shape(edges)[0], 1))
+            with tf.variable_scope('encoder', reuse=reuse):
+                x = layers.embed_sequence(nodes, vocab_size=len(self.data.dict), embed_dim=self.n_hidden)
+                x0 = x
+
+                with tf.variable_scope('steps'):
+                    for step in range(self.n_steps):
+                        x = message_passing(x, edges, edge_features, lambda x: mlp(x, 'message-fn'))
+                        x = mlp(tf.concat([x, x0], axis=1), 'post')
+                        tf.get_variable_scope().reuse_variables()
+
+                return tf.gather(x, heads)
+
+        ea = encode_graph(nodes_a, edges_a, segments_a, heads_a, False)
+        eb = encode_graph(nodes_b, edges_b, segments_b, heads_b, True)
+        logits = mlp(tf.concat([ea, eb], axis=1), "logits", n_out=1)
+        log_loss = tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.reshape(targets, (-1, 1)), logits=logits) / tf.log(2.)
+
+        reg_loss = sum(tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES))
+        tf.summary.scalar('reg_loss', reg_loss)
+
+        self.loss = tf.reduce_mean(log_loss) + reg_loss
+        tf.summary.scalar('loss', self.loss)
+
+        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        with tf.control_dependencies(update_ops):
+            gvs = self.optimizer.compute_gradients(self.loss, colocate_gradients_with_ops=True)
+            self.train_step = self.optimizer.apply_gradients(gvs, global_step=self.global_step)
+
+        for g, v in gvs:
+            tf.summary.histogram("grads/" + v.name, g)
+            tf.summary.histogram("vars/" + v.name, v)
+            tf.summary.histogram("g_ratio/" + v.name, g / (v + 1e-8))
+
+        self.session.run(tf.global_variables_initializer())
+        self.saver = tf.train.Saver()
+        util.print_vars(tf.trainable_variables())
+
+        tensorboard_dir = os.environ.get('TENSORBOARD_DIR') or '/tmp/tensorboard'
+        self.train_writer = tf.summary.FileWriter(tensorboard_dir + '/entail/%s/train/%s' % (self.revision, self.name), self.session.graph)
+        self.test_writer = tf.summary.FileWriter(tensorboard_dir + '/entail/%s/test/%s' % (self.revision, self.name), self.session.graph)
+        self.summaries = tf.summary.merge_all()
+
+    def train_batch(self):
+        step = self.session.run(self.global_step)
+        if step % 1000 == 0:
+            _, loss, summaries = self.session.run([self.train_step, self.loss, self.summaries], {self.is_training_ph: True})
+            self.train_writer.add_summary(summaries, step)
+            self.train_writer.flush()
+        else:
+            _, loss = self.session.run([self.train_step, self.loss], {self.is_training_ph: True})
+        return loss
+
+    def val_batch(self):
+        loss, summaries, step = self.session.run([self.loss, self.summaries, self.global_step], {self.is_training_ph: False})
+        self.test_writer.add_summary(summaries, step)
+        self.test_writer.flush()
+        return loss
+
+    def save(self, name):
+        self.saver.save(self.session, name)
+
+    def load(self, name):
+        print("Loading %s..." % name)
+        self.saver.restore(self.session, name)
+
+    def _iterator(self, generator, output_types, output_shapes):
+        return Dataset.from_generator(
+            generator,
+            output_types,
+            output_shapes
+        ).prefetch(1).make_one_shot_iterator()
+
+
+if __name__ == '__main__':
+    m = EntailmentRRN()
+    print(m.train_batch())
+    print(m.val_batch())
