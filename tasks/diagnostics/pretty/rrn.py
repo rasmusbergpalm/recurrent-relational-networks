8744c1209214f7432dca7e6ebda565e08b9136bb
Rasmus Berg Palm <rasmusbergpalm@gmail.com>
2018-04-06 13:11:13 +0200

    pretty, scale+rotate, 1x dropout, no LSTM

--- tasks/diagnostics/pretty/rrn.py
+++ tasks/diagnostics/pretty/rrn.py
@@ -20,7 +20,7 @@ import matplotlib.pyplot as plt
 
 
 class PrettyRRN(Model):
-    number = 4
+    number = 2
     batch_size = 128
     revision = os.environ.get('REVISION')
     message = os.environ.get('MESSAGE')
