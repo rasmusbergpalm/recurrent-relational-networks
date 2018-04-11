71a408eccf8fb6d18dae4adefb28c103eb5662b5
Rasmus Berg Palm <rasmusbergpalm@gmail.com>
2018-04-11 11:25:03 +0200

    test no dropout

--- tasks/babi/rrn.py
+++ tasks/babi/rrn.py
@@ -30,7 +30,6 @@ class BaBiRecurrentRelationalNet(Model):
     n_steps = 3
     edge_keep_prob = 1.0
     n_hidden = 128
-    pretrained = None
 
     def __init__(self, is_testing):
         super().__init__()
@@ -115,12 +114,12 @@ class BaBiRecurrentRelationalNet(Model):
                         def graph_fn(x):
                             with tf.variable_scope('graph-fn'):
                                 x = layers.fully_connected(x, self.n_hidden, weights_regularizer=regularizer)
-                                x = layers.dropout(x, is_training=self.is_training_ph)
+                                # x = layers.dropout(x, is_training=self.is_training_ph)
                                 x = layers.fully_connected(x, self.n_hidden, weights_regularizer=regularizer)
-                                x = layers.dropout(x, is_training=self.is_training_ph)
+                                # x = layers.dropout(x, is_training=self.is_training_ph)
                                 return layers.fully_connected(x, self.vocab.size(), activation_fn=None, weights_regularizer=regularizer)
 
-                        x = f_encoding
+                        x = tf.concat([f_encoding, tf.gather(q_encoding, fact_segments_ph)], 1)
                         x = mlp(x, 'pre', self.n_hidden)
                         edge_features = tf.gather(q_encoding, edge_segments_ph)
                         x0 = x
@@ -172,9 +171,6 @@ class BaBiRecurrentRelationalNet(Model):
 
             self.session.run(tf.global_variables_initializer())
             self.saver = tf.train.Saver()
-            if self.pretrained is not None:
-                self.load('../' + self.pretrained + '/best')
-
             util.print_vars(tf.trainable_variables())
 
             tensorboard_dir = os.environ.get('TENSORBOARD_DIR') or '/tmp/tensorboard'
