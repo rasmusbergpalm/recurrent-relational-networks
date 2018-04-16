3e244b9359ef71842a05b6257ba52df2eb006cb7
Rasmus Berg Palm <rasmusbergpalm@gmail.com>
2018-04-16 15:34:25 +0200

    wip

--- tasks/diagnostics/pretty/test.py
+++ tasks/diagnostics/pretty/test.py
@@ -1,7 +1,7 @@
 from tasks.diagnostics.pretty.rrn import PrettyRRN
 
-rrn_4_steps = "0ac07d0"
-rrn_1_step = "b7e022e"
+rrn_4_steps = "../0ac07d0/best"
+rrn_1_step = "../b7e022e/best"
 
 m = PrettyRRN()
 m.load(rrn_4_steps)
